a319626cde45d5d8612f971672dd3ecd795aaef8
JEONGHYUNK-328\JeonghyunK <kimjh@bawi.org>
2018-01-22 16:38:44 +0900

    add Classification model and codes

--- models.py
+++ models.py
@@ -4,23 +4,25 @@ from keras.layers import Activation, BatchNormalization, Conv2D, Conv2DTranspose
 import h5py
 import numpy as np
 
-def CNNClassifier(k):
+def CNNClassifier(k, input_shape):
     model = Sequential()
-    model.add(Conv2D(k, (3, 3), padding='same', activation='relu', input_shape=input_shape))
-    model.add(Conv2D(k, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(k, (3, 3), padding='same', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Conv2D(k, (3, 3)))
+    model.add(Activation('relu'))
 
-    model.add(Conv2D(k, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(k, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(k, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(k, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(2 * k, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(2 * k, (3, 3)))
+    model.add(Activation('relu'))
 
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dense(nClasses, activation='softmax'))
+    model.add(Dense(k))
+    model.add(Activation('relu'))
+    model.add(Dense(2))
+    model.add(Activation('softmax'))
+
+    return model
 
 def DenseResNet14(k):
     # Create Model
